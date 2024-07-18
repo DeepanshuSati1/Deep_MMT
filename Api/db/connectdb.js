b038b340ff9e78034c8e5ac7cea7c8b23454aaa6
DeepanshuSati1 <161916551+DeepanshuSati1@users.noreply.github.com>
2024-07-18 17:29:12 +0530

    Update connectdb.js

--- Api/db/connectdb.js
+++ Api/db/connectdb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect("mongodb+srv://deepanshusati238:Xuf4r1pqy7Ot4AX2@makemytrip.wnisfjk.mongodb.net/makemytrip?retryWrites=true&w=majority&appName=makemytrip");
     console.log(`> Successfully connnected to mongoDB`);
   } catch (error) {
     console.error(`> ERROR: ${error.message}`);
